d8f14752cc38fea69c975ff7a094e24990780a4e
Juyoung35 <justin350518@gmail.com>
2022-12-13 06:36:49 +0900

    add maze.h

---
+++ src/maze.h
@@ -0,0 +1,32 @@
+typedef struct point {
+	int x;
+	int y;
+} point;
+
+typedef struct squared_maze {
+	int width;
+	int height;
+	int **data;
+	point start;
+	point end;
+} squared_maze;
+
+typedef struct maze {
+	int n;
+	int radius;
+	int **data;
+	point start;
+	point end;
+} maze;
+
+void generate_squared_maze(int width, int height);
+
+void generate_maze(int n, int radius);
+
+int solve_squared_maze(squared_maze maze_);
+
+int solve_maze(maze maze_);
+
+void print_squared_maze(squared_maze maze_);
+
+void print_maze(maze maze_);
